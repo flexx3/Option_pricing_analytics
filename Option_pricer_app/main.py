00beb10dbfca53a94f01279b6581e30c4699828b
Felix Obioma Nkwuzor <felixobioma99@gmail.com>
2025-06-07 07:50:52 +0100

    Update main.py

--- Option_pricer_app/main.py
+++ Option_pricer_app/main.py
@@ -72,4 +72,4 @@ app.layout = dbc.Container([
 
 #run app
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=False, host="0.0.0.0", port=8050, use_reloader=False)
